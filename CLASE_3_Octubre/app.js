3bf86a7a18346cc91cfeda13ecdfefd22b05eab8
Andres <andguja@gmail.com>
2023-10-05 18:48:42 -0500

    5 octubre

--- CLASE_3_Octubre/app.js
+++ CLASE_3_Octubre/app.js
@@ -9,13 +9,30 @@ app.listen(3000,()=>{
     console.log("el servidor se esta ejecutando en el puerto 3000" .green)
 })
 
+app.use(express.urlencoded({extended:false}))
+app.use(express.json())
+
+
+
 
 app.get("/suma",(req,res)=>{
 
+console.log(req.body) 
+let suma= req.body.numero1+req.body.numero2
+if(Number(suma))
     
-res.send({name:"apliaccion web"})
+res.send({Suma:req.body.numero1+req.body.numero2})
+
     
 })
+app.get("/add/:numero1/:numero2",(req,res)=>{
+
+    console.log(req.params) 
+    
+        
+    res.send({Suma:req.params.numero1+req.params.numero2})
+        
+    })
 
 
 console.log("El resultado es " .brightMagenta)
