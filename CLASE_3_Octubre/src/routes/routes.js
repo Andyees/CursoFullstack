c03fd6e9b3d2e3cca21193047fe94a73f0508442
Andres <andguja@gmail.com>
2023-10-17 19:55:12 -0500

    17 octubre clase

--- CLASE_3_Octubre/src/routes/routes.js
+++ CLASE_3_Octubre/src/routes/routes.js
@@ -29,11 +29,7 @@ res.sendFile("./index.html",{ root: __dirname })
 
 })
 
-router.get("/main",async(req,res)=>{
-  const ListUser=await users.find()
-  res.render("users",{Usuarios:ListUser})
 
-})
 
 
 router.get("/main/addUser",(req,res)=>{
@@ -69,40 +65,48 @@ router.get("/main/DeleteUser",(req,res)=>{
 
 // })
 
-router.post("/main/DeleteUser",async(req,res)=>{
+// router.post("/main/DeleteUser",async(req,res)=>{
 
-  // Leer datos Data.json 
- let usuarios= await funciones.read('Functions/data.json')
- //Eliminar usuario al Arreglo 
-let IdEliminar=req.body.id
+//   // Leer datos Data.json 
+//  let usuarios= await funciones.read('Functions/data.json')
+//  //Eliminar usuario al Arreglo 
+// let IdEliminar=req.body.id
 
-const existe= usuarios.some(usuario=> usuario.id==IdEliminar)
-console.log(existe)
-if (existe) {
-  let Newusuarios= await usuarios.filter(usuario=>usuario.id!=IdEliminar)
+// const existe= usuarios.some(usuario=> usuario.id==IdEliminar)
+// console.log(existe)
+// if (existe) {
+//   let Newusuarios= await usuarios.filter(usuario=>usuario.id!=IdEliminar)
 
  
-  //Escribir en el archivo Data.json
-  let StateWriter=await funciones.write(Newusuarios,"Functions/data.json")
+//   //Escribir en el archivo Data.json
+//   let StateWriter=await funciones.write(Newusuarios,"Functions/data.json")
   
-  //Validacion de la escritura
-    if(StateWriter){
-      console.log("El cliente fue Eliminado exitosamente")
-      res.redirect("/main")
-    }
-    else{
-      res.sendStatus(403)
+//   //Validacion de la escritura
+//     if(StateWriter){
+//       console.log("El cliente fue Eliminado exitosamente")
+//       res.redirect("/main")
+//     }
+//     else{
+//       res.sendStatus(403)
   
-  }
+//   }
 
-} else {
+// } else {
 
-res.render("DeleteUser_DontExist")
+// res.render("DeleteUser_DontExist")
 
   
-}})
+// }})
 
 // Endpoints DATABASE
+
+
+router.get("/main",async(req,res)=>{
+  const ListUser=await users.find()
+  res.render("users",{Usuarios:ListUser})
+
+})
+
 router.post("/main/addUser",(req,res)=>{
 
     const userDif={"id":req.body.id,"Name":req.body.name}
@@ -119,9 +123,57 @@ router.post("/main/addUser",(req,res)=>{
  
  router.post("/main/DeleteUser",(req,res)=>{
  
-   
- 
-   
+  let IdEliminar=req.body.id
+
+  users.deleteOne({"id":IdEliminar}).then((resp)=>{
+
+    console.log(resp.deletedCount)
+    
+    if(resp.deletedCount===0){
+      res.render("DeleteUser_DontExist",{id:IdEliminar})
+     console.log("Usuario no encontrado")
+    }
+    else{
+      console.log("El usuario fue eliminado exitosamente")
+      res.redirect("/main")
+    }
+
+  })  
+ })
+
+ router.get("/main/ViewEdit/:id", async(req,res)=>{
+
+       let id=req.params.id
+       let usuario= await users.findOne({"id":id})
+       console.log(usuario.Cargo)
+       res.render("EditUser",{user:usuario})
+
+ })
+
+ router.post("/main/editUser",async(req,res)=>{
+
+  let usuario=req.body
+  console.log(usuario)
+  let resp=  await users.updateOne({"id":usuario.id},usuario)
+
+    if(resp.modifiedCount===0){
+
+      console.log("el usuario no existe")
+      
+    }
+
+    else{
+
+      console.log("el usuario ha sido actualizado con exito")
+      res.redirect("/main")
+    }
+
+
+
+
+
+
+
  })
 
 
